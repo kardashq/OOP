41d54290aecbc868dcfcc3f7f3b7c132454416a5
kardashq <kardashq@yandex.ru>
2022-08-07 22:56:51 +0300

    hometask add

--- task_1.py
+++ task_1.py
@@ -1,1 +1,8 @@
-
+class Nobody():
+    def __init__(self, name):
+        if name.lower() == 'nobody':
+            self.name = name
+        else:
+            self.name = "I'm not nobody, " + " " + str(name)
+people = Nobody('Nobody')
+print(people.name)
