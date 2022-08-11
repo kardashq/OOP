cfe6c3e7698faf29440daea0b85da037745be235
kardashq <kardashq@yandex.ru>
2022-08-11 22:30:56 +0300

    adding HW task_4.py

---
+++ task_4.py
@@ -0,0 +1,16 @@
+class Kg():
+    def __init__(self: float):
+        pass
+    def tonn_into_kg(self):
+        return print(self * 1000)
+    def centner_into_kg(self):
+        return print(float(self * 100))
+    def funt_into_kg(self):
+        return print(float(self * 2.20462))
+    def pud_into_kg(self):
+        return print(float(self * 0.0610475))
+Kg.tonn_into_kg(1)
+Kg.centner_into_kg(2)
+Kg.funt_into_kg(3)
+Kg.pud_into_kg(30)
+
